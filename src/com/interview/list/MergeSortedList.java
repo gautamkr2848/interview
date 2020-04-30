91ff95e7dc24d8572b476eb47ce91b2f1501f76d
Gautam.Kumar <gautam.kumar@oaknorth.ai>
2020-04-30 13:32:39 +0530

    merge sorted list

---
+++ src/com/interview/list/MergeSortedList.java
@@ -0,0 +1,38 @@
+package com.interview.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MergeSortedList {
+
+    public void mergeSortedList(){
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(5);
+        list1.add(7);
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(1);
+        list2.add(2);
+        list2.add(3);
+        list2.add(4);
+
+        List<Integer> list3 = new ArrayList<>();
+
+        int i=0;
+        int j=0;
+        while(i<list1.size() && j<list2.size()){
+            if(list1.get(i) < list2.get(j))
+                list3.add(list1.get(i++));
+            else
+                list3.add(list2.get(j++));
+        }
+
+        while(i< list1.size())
+            list3.add(list1.get(i++));
+        while (j < list2.size())
+            list3.add(list2.get(j++));
+
+        System.out.println(list3.toString());
+    }
+}
