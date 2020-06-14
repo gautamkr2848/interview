ae80d4cf1841193cf7ae3bd16b04baeb6dc27087
Gautam.Kumar <gautam.kumar@oaknorth.ai>
2020-06-14 13:17:20 +0530

    tree

--- src/com/interview/tree/MaxElement.java
+++ src/com/interview/tree/MaxElement.java
@@ -2,14 +2,14 @@ package com.interview.tree;
 
 public class MaxElement {
 
-    public int maxElement(BinaryTree node){
+    public int maxElement(Node node){
 
         int max = 0;
         if(node != null){
-            int leftMax = maxElement(node.getLeft());
-            int rightMax = maxElement(node.getRight());
+            int leftMax = maxElement(node.left);
+            int rightMax = maxElement(node.right);
             max = leftMax > rightMax ? leftMax : rightMax;
-            max = node.data > max ? node.data : max;
+            max = node.key > max ? node.key : max;
         }
         return max;
     }
