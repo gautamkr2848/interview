7f0be7cefc83c55bfe43da044716e1b592dfcdd2
Gautam.Kumar <gautam.kumar@oaknorth.ai>
2020-06-15 02:49:04 +0530

    Linked list

--- src/com/interview/list/RemoveLoop.java
+++ src/com/interview/list/RemoveLoop.java
@@ -1,15 +1,5 @@
 package com.interview.list;
 
-class Node {
-    public int data;
-    public Node next;
-
-    public Node(int d) {
-        data = d;
-        next = null;
-    }
-}
-
 public class RemoveLoop {
 
     public int detectAndRemoveLoop(Node node) {
